ee086284be37dd10792bb81953c32abaeaacb4af
Mark Lysenko <oomarkizoo@mail.ru>
2020-09-11 20:19:31 +0300

    Optimizes the code, adds a new field for the final product price
    Also adds migration to it.

--- app_shop/models/trade_models.py
+++ app_shop/models/trade_models.py
@@ -6,16 +6,24 @@ from tinymce.models import HTMLField
 from shop_allhere.utils import transliterate
 
 
+PRODUCT_DISCOUNT = "Enter the discount percentage for this product"
+PRODUCT_DISCOUNT_FIXED_PRICE = "Enter the price of the fixed discount"
+PROMOTIONS_TIME = "Use an interactive calendar image or enter a date in the format 'YYYY-MM-DD'"
+PROMOTIONS_FOR_CAROUSEL = "Check, if promotion should be used for the main carousel"
+
+
 class Product(models.Model):
     name = models.CharField(max_length=100, help_text="Enter product name")
     price = models.FloatField(help_text="Enter product price")
+    the_final_price = models.FloatField(editable=False, null=True, blank=True)
     description = HTMLField(help_text="Enter a store description", null=True, blank=True)
     images = models.ImageField(upload_to="products")
-    discount = models.PositiveSmallIntegerField(
-        default=None, help_text="Enter the discount percentage for this product", null=True, blank=True)
-    discount_fixed_price = models.FloatField(help_text="Enter the price of the fixed discount", null=True, blank=True)
-    classification = models.ForeignKey(
-        'ProductClassification', on_delete=models.CASCADE, related_name='class_content', null=True, blank=True)
+    discount = models.PositiveSmallIntegerField(default=None, help_text=PRODUCT_DISCOUNT,
+                                                null=True, blank=True)
+    discount_fixed_price = models.FloatField(help_text=PRODUCT_DISCOUNT_FIXED_PRICE,
+                                             null=True, blank=True)
+    classification = models.ForeignKey('ProductClassification', on_delete=models.CASCADE,
+                                       related_name='class_content', null=True, blank=True)
     specifications = JSONField(encoder={}, null=True, blank=True)
     slug = models.CharField(max_length=120, editable=False, null=True, blank=True)
 
@@ -47,6 +55,7 @@ class Product(models.Model):
             return None
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        self.the_final_price = self.get_current_prices()
         if not self.slug:
             self.slug = transliterate(self.name)
         return super(Product, self).save()
@@ -133,43 +142,13 @@ class Promotions(models.Model):
     name = models.CharField(max_length=100, help_text="Enter product name")
     description = HTMLField(help_text="Enter a store description", null=True, blank=True)
     images = models.ImageField(upload_to="promotions")
-    start_time = models.DateField(
-        help_text="Use an interactive calendar image or enter a date in the format 'YYYY-MM-DD'", null=True, blank=True)
-    end_time = models.DateField(
-        help_text="Use an interactive calendar image or enter a date in the format 'YYYY-MM-DD'", null=True, blank=True)
-    for_carousel = models.BooleanField(
-        help_text="Check, if promotion should be used for the main carousel", default=False)
+    start_time = models.DateField(help_text=PROMOTIONS_TIME, null=True, blank=True)
+    end_time = models.DateField(help_text=PROMOTIONS_TIME, null=True, blank=True)
+    for_carousel = models.BooleanField(help_text=PROMOTIONS_FOR_CAROUSEL, default=False)
     for_category = models.ManyToManyField('ProductClassification', through='PromotionsForCategory')
 
     def __str__(self):
         return '{0} ({1} - {2})'.format(self.name, self.start_time, self.end_time)
-
-    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
-        super(Promotions, self).save()
-        category_list = PromotionsForCategory.objects.filter(promotion_id=self.id)
-        for item in category_list:
-            subcategories_qs = ProductClassification.objects.filter(category_id=item.category.id)
-            if subcategories_qs:
-
-                def recursive_get(item):
-                    if item.category:
-                        child_list = ProductClassification.objects.filter(category_id=item.id)
-                        for child in child_list:
-                            id_list.add(child.id)
-                            recursive_get(item.category)
-                    else:
-                        return id_list
-
-                for obj in subcategories_qs:
-                    id_list = {obj.id}
-                    recursive_get(obj)
-
-                    for num in id_list:
-                        PromotionsForCategory.objects.get_or_create(promotion_id=self.id,
-                                                                    category_id=num,
-                                                                    discount=item.discount)
-
-        return super(Promotions, self).save()
 
     def list_categories(self):
         return self.objects.for_category
