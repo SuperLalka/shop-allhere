9a8e6294a901d2da3f3189d6d745fa246a532f78
Mark Lysenko <oomarkizoo@mail.ru>
2020-09-16 12:07:33 +0300

    Adds to the context processor getting the object of the current store
    Also adds the city for the default store to constants file

--- allhere_in_russia/context_processors.py
+++ allhere_in_russia/context_processors.py
@@ -5,6 +5,7 @@ from shop_allhere import settings
 
 def request(request):
     shops_list = Shops.objects.all()
+    current_store = Shops.objects.get(id=request.session['shop'])
     city_list = shops_list.values_list('city', flat=True).distinct().order_by('city')
     return {
         'API_KEY_YANDEX_GEOCODER': settings.API_KEY_YANDEX_GEOCODER,
@@ -13,5 +14,6 @@ def request(request):
         'for_landlords_form': ForLandlordsForm(),
         'for_lease_holders_form': ForLeaseHoldersForm(),
         'shops_list': shops_list,
+        'current_store': current_store,
         'city_list': city_list,
     }
